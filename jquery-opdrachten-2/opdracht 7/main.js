b6081cca26d2eaea981b2ad44a44be66cd106812
dominicho97 <dominic.ho97@gmail.com>
2019-02-04 15:04:07 +0100

    finished 5 of 7lines, unfinished

--- jquery-opdrachten-2/opdracht 7/main.js
+++ jquery-opdrachten-2/opdracht 7/main.js
@@ -10,7 +10,17 @@ $("ol li:nth-child(2)").click(function() {
   $("ol li:nth-child(2)").css("font-size", "80%");
 });
 
-// sets third chld of ol li to bold
+// changes words with class green to bold
 $("ol li:nth-child(3)").click(function() {
-  $("ol li:nth-child(3)").css("font-weight", "bold");
+  $(".green").css("font-weight", "bold");
+});
+
+// sets word with class red in underline
+$("ol li:nth-child(4)").click(function() {
+  $(".red").css("text-decoration", "underline");
+});
+
+// sets word with class red in underline//??
+$("ol li:nth-child(5)").mouseover(function() {
+  $("#content").css("content", "images/koala.png");
 });

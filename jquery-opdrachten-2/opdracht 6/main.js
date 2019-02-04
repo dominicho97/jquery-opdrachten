8352e4a36efb3fab522523bb6b93827621c9d5ec
dominicho97 <dominic.ho97@gmail.com>
2019-02-04 14:11:42 +0100

    finished excercise

---
+++ jquery-opdrachten-2/opdracht 6/main.js
@@ -0,0 +1,29 @@
+// turn text to green on mouseover
+$("#green").mouseover(function() {
+  $("#text").css("color", "green");
+});
+
+// turn text to black on mouseleave
+$("#green").mouseleave(function() {
+  $("#text").css("color", "black");
+});
+
+// turn text to red on mouseover
+$("#red").mouseover(function() {
+  $("#text").css("color", "red");
+});
+
+// turn text to black on mouseleave
+$("#red").mouseleave(function() {
+  $("#text").css("color", "black");
+});
+
+// turn text to blue on mouseover
+$("#blue").mouseover(function() {
+  $("#text").css("color", "blue");
+});
+
+// turn text to black on mouseleave
+$("#blue").mouseleave(function() {
+  $("#text").css("color", "black");
+});

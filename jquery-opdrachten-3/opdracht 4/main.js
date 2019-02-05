22c09665f6e46be552a52dc94cefc42948d0b6cf
dominicho97 <dominic.ho97@gmail.com>
2019-02-05 12:16:55 +0100

    finished excercise

--- jquery-opdrachten-3/opdracht 4/main.js
+++ jquery-opdrachten-3/opdracht 4/main.js
@@ -1,3 +1,24 @@
+// adds 10px to the rectangle
 $(".btn1").click(function() {
   $("#rectangle").css("height", "100px");
+});
+
+// changes color to green
+$(".btn2").click(function() {
+  $("#rectangle").css("background", "green");
+});
+
+// reset color to red
+$(".btn3").click(function() {
+  $("#rectangle").css("background", "red");
+});
+
+// hide rectangle
+$(".btn4").click(function() {
+  $("#rectangle").hide();
+});
+
+// show rectangle
+$(".btn5").click(function() {
+  $("#rectangle").show();
 });

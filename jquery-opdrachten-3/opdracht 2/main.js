cb8d9f884c4c05575f9af2b8537d71e723a25c49
dominicho97 <dominic.ho97@gmail.com>
2019-02-05 10:50:29 +0100

    added min button, not working, unfinished

--- jquery-opdrachten-3/opdracht 2/main.js
+++ jquery-opdrachten-3/opdracht 2/main.js
@@ -5,3 +5,11 @@ button.onclick = function() {
   count += 1;
   button.innerHTML = " " + count;
 };
+
+//-button
+let button2 = document.getElementById("min"),
+  count2 = 0;
+button2.onclick = function() {
+  count = -1;
+  button2.innerHTML = " " + count2;
+};

e26f6a48048ef6106222d5e7dc9e8060b7ff8f2c
dominicho97 <dominic.ho97@gmail.com>
2019-02-06 14:23:39 +0100

    moved button outside form

---
+++ jquery-opdrachten-4/opdracht 3/main.js
@@ -0,0 +1,15 @@
+/*$("document").ready(function() {
+  let input1 = $("#input1");
+
+  function myFunction() {
+    console.log(input1.val());
+  }
+});*/
+
+let button = document
+  .getElementById("button1")
+  .addEventListener("click", myFunction);
+
+function myFunction() {
+  console.log("test");
+}
